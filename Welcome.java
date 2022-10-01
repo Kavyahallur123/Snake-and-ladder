a47bcdd61d90c338c2d0da4852163f22b1db9675
kavyahallur123 <kavyahallur199@gmail.com>
2022-10-01 14:29:30 +0530

    Welcome to snake and ladder program

---
+++ Welcome.java
@@ -0,0 +1,7 @@
+
+package com.bridgelab.snakeandldder;
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Wel-Come to Snake and ladder program");
+    }
+}
