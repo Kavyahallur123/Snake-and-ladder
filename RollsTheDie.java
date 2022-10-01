e0d9d72365a04eb74c6f66e5833af48d5a722ba4
kavyahallur123 <kavyahallur199@gmail.com>
2022-10-01 14:06:03 +0530

    Generating dice value between 1-6 using random class

---
+++ RollsTheDie.java
@@ -0,0 +1,17 @@
+/*The Player rolls the die to get a number between 1 to 6. - Use ((RANDOM)) to get the
+number between 1 to 6*/
+
+
+package com.bridgelab.snakeandldder;
+
+public class RollsTheDie {
+    public static void main(String[] args) {
+        RollsTheDie a = new RollsTheDie();
+        a.rollDie();
+
+    }
+    public void rollDie(){
+        int die = (int) Math.floor(Math.random() * 10) % 6 + 1;
+        System.out.println("Die value is " + die);
+    }
+}
