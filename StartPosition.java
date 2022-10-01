4a752d2c51f9b9969eb963c2d935e2a027e15471
kavyahallur123 <kavyahallur199@gmail.com>
2022-10-01 14:33:16 +0530

    Player is at starting position

---
+++ StartPosition.java
@@ -0,0 +1,11 @@
+/*Snake and Ladder game played with single player at start position 0*/
+
+package com.bridgelab.snakeandldder;
+
+public class StartPosition {
+    public static void main(String[] args) {
+        int playerPosition = 0;
+        System.out.println("Position of player is " + playerPosition);
+    }
+
+}
